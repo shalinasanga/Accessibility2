00bdb2af3f70b8158308351bcd42a9b496e9a9fa
shalin <shalin@embla.asia>
2021-02-25 15:48:43 +0530

    Initial commit

---
+++ Accessibility/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Accessibility
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new listpage());
+        }
+    }
+}
---
+++ Accessibility/listpage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Accessibility
+{
+    public partial class listpage : ContentPage
+    {
+        public listpage()
+        {
+            InitializeComponent();
+            ObservableCollection<DetailEvent> names = new ObservableCollection<DetailEvent>();
+            for (int i = 0; i < 10; i++)
+            {
+                names.Add(new DetailEvent
+                {
+                    Name = "items" + i.ToString()
+                });
+            }
+            stringlist.ItemsSource = names;
+        }
+
+        void SwipeItemView_Invoked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+
+    public class DetailEvent : INotifyPropertyChanged
+    {
+
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
